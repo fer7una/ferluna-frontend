64541fb250582d19e55df71f14f4da1df966d83a
fer7una <fernan.luna752001@gmail.com>
2026-05-27 00:33:04 +0200

    Spinnin

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { applyMoonPhaseFavicon } from "./useMoonPhaseFavicon";
 import "./styles.css";
+
+applyMoonPhaseFavicon();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
